26557623bea751d0b30bb5da7a7834155e4ae8b7
Unnikrishnan <uknamboodiri@gmail.com>
2020-06-05 13:32:03 +0530

    restructured

--- convert.py
+++ convert.py
@@ -1,28 +1,22 @@
-"""Program to convert all JPG files from a given folder & save them to a output folder as PNG
+# Goal is to convert jpg images from a given folder to png and save them in a spearate folder
+# accept 2 images folder and new folder name at console
 
-Usage:
-JPGtoPNGconverter <input_folder> <output_folder>
-
-Example:
-      python JPGtoPNGconverter.py ./images ./png_images
-"""
 import sys
 import os
-from PIL import Image
+from PIL import Image, ImageFilter
 
-# take input and out folder location as input
 input_folder = sys.argv[1]
 output_folder = sys.argv[2]
+
+print(input_folder, output_folder)
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 for item in os.listdir(input_folder):
-    img = Image.open(f"{input_folder}/{item}")
-    file_name = os.path.splitext(item)[0]
+    img = Image.open(os.path.join(input_folder, item))
+    file, ext = os.path.splitext(item)
+    img.save(f'{output_folder}/{file}.png', 'png')
 
-    img_thumbnail = Image.open(f"{input_folder}/{item}")
-    img_thumbnail.thumbnail((100, 100))
-
-    img.save(f"{output_folder}/{file_name}.png", "png")
-    img_thumbnail.save(f"{output_folder}/{file_name}_thumbail.png", "png")
+    img.thumbnail((100, 100))
+    img.save(f'{output_folder}/{file}_thumbnail.png', 'png')
